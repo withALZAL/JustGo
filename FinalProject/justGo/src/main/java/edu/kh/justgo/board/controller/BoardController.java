220065b101932761c1a3948fb56209860cfe9879
workspace <meso8587@naver.com>
2023-06-12 12:25:01 +0900

    boardcontroller 수정

--- FinalProject/justGo/src/main/java/edu/kh/justgo/board/controller/BoardController.java
+++ FinalProject/justGo/src/main/java/edu/kh/justgo/board/controller/BoardController.java
@@ -1,5 +1,12 @@
 package edu.kh.justgo.board.controller;
 
-public class BoardController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+@SessionAttributes({"loginMember"})
+@RequestMapping("/board")
+@Controller
+public class BoardController {
+	
 }
