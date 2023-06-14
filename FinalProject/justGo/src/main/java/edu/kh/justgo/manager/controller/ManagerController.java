70dddf87c11422d8ea20f2ea21dbb640976a3cff
정송이 <pso7364@naver.com>
2023-06-14 10:37:09 +0900

    관리자 회원관리 목록 데이터 불러오기 구현 중

--- FinalProject/justGo/src/main/java/edu/kh/justgo/manager/controller/ManagerController.java
+++ FinalProject/justGo/src/main/java/edu/kh/justgo/manager/controller/ManagerController.java
@@ -1,11 +1,17 @@
 package edu.kh.justgo.manager.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.kh.justgo.manager.model.service.ManagerService;
+import edu.kh.justgo.member.model.dto.Member;
 
 @Controller
 @RequestMapping("/manager")
@@ -14,13 +20,27 @@ public class ManagerController {
 	@Autowired
 	private ManagerService service;
 	
+	
 	// 회원관리 관리자페이지 연결
 	@GetMapping("/memberManager")
-	public String memberManager() {
+	public String selectMemberList(
+			@RequestParam(value="cp", required=false, defaultValue="1") int cp
+			, Model model
+			) {
+		
+		// 회원정보 불러오기
+		Map<String, Object> map = service.selectMemberList(cp);
+		
+		model.addAttribute("map", map);
+		
+		// 콘솔에서 확인
+//		System.out.println(memberList);
+		
 		return "/manager/memberManager";
 	}
 	
 	
+
 	
 	
 	// 1:1문의 관리자페이지 연결
--- FinalProject/justGo/src/main/java/edu/kh/justgo/manager/model/dao/ManagerDAO.java
+++ FinalProject/justGo/src/main/java/edu/kh/justgo/manager/model/dao/ManagerDAO.java
@@ -1,14 +1,47 @@
 package edu.kh.justgo.manager.model.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import edu.kh.justgo.manager.model.dto.Pagination;
+import edu.kh.justgo.member.model.dto.Member;
 
 
 @Repository
 public class ManagerDAO {
 	
+
 	@Autowired
-	private SqlSessionTemplate sqlSession;
+	private SqlSessionTemplate sql;
+
+	
+	public int getMemberListCount() {
+		return sql.selectOne("managerMapper.getMemberListCount");
+	}
+	
+	
+	
+	public List<Member> selectMemberList(Pagination pagination) {
+		
+		int offset = (pagination.getCurrentPage() - 1 ) * pagination.getLimit();
+		RowBounds rowBounds = new RowBounds(offset, pagination.getLimit());
+		return sql.selectList("managerMapper.selectMemberList", null, rowBounds);
+		
+	}
+
+
+
+
+
+
+	
+	
+	
+	
 
 }
