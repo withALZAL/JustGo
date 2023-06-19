51fe088aded98209676950ac2883c83af6ffd6ba
woohyeongjo <anya92@naver.com>
2023-06-19 13:55:25 +0900

    댓글 초안

--- FinalProject/justGo/src/main/java/edu/kh/justgo/board/controller/ReplyController.java
+++ FinalProject/justGo/src/main/java/edu/kh/justgo/board/controller/ReplyController.java
@@ -1,5 +1,26 @@
 package edu.kh.justgo.board.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.kh.justgo.board.model.dto.Reply;
+import edu.kh.justgo.board.model.service.ReplyService;
+
+@RestController
 public class ReplyController {
 
+	@Autowired
+	private ReplyService service;
+	
+	@GetMapping(value="/comment",produces="application/json; charset=UTF-8")
+	public List<Reply> select(@RequestParam("boardNo")int boardNo){
+		return service.select(boardNo);
+	}
+	
+	
+	
 }
--- FinalProject/justGo/src/main/java/edu/kh/justgo/board/model/service/ReplyServiceImpl.java
+++ FinalProject/justGo/src/main/java/edu/kh/justgo/board/model/service/ReplyServiceImpl.java
@@ -1,5 +1,25 @@
 package edu.kh.justgo.board.model.service;
 
-public class ReplyServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.kh.justgo.board.model.dao.ReplyDAO;
+import edu.kh.justgo.board.model.dto.Reply;
+
+@Service
+public class ReplyServiceImpl implements ReplyService {
+
+	
+	@Autowired
+	private ReplyDAO dao;
+
+	@Override
+	public List<Reply> select(int boardNo) {
+		
+		return dao.select(boardNo);
+	}
+	
+	
 }
