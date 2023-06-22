067d6aec70c9823178daa80e29ae6248718cf020
sangzoon0102 <sangzoon0102@naver.com>
2023-06-22 17:03:13 +0900

    ㄹㅇ

--- FinalProject/justGo/src/main/webapp/resources/js/account/updateNickname.js
+++ FinalProject/justGo/src/main/webapp/resources/js/account/updateNickname.js
@@ -14,7 +14,7 @@ memberNickname.addEventListener("input", () => {
         memberNickname.value = "";       
         nickMessage.classList.add("error");
         nickMessage.classList.remove("confirm");
-        nickMessage.innerText = "닉네임을 입력하세요";
+        nickMessage.innerText = "닉네임을 입력하세요.";
         checkObj.memberNickname = false;
         return;
     } 
@@ -25,26 +25,21 @@ memberNickname.addEventListener("input", () => {
         .then(resp => resp.text()) 
         .then(count => {
             if(count == 0){ 
-
-        nickMessage.innerText = "사용 가능한 닉네임입니다.";
-        nickMessage.classList.add("confirm");
-        nickMessage.classList.remove("error");
-        checkObj.memberNickname = true;
-
-        } else { // 무효
-            nickMessage.innerText = "이미 사용 중인 닉네임입니다.";
-            nickMessage.classList.add("error");
-            nickMessage.classList.remove("confirm");
-            checkObj.memberNickname = false;
-        }
-
-    })
-
+                nickMessage.innerText = "사용 가능한 닉네임입니다.";
+                nickMessage.classList.add("confirm");
+                nickMessage.classList.remove("error");
+                checkObj.memberNickname = true;
+            } else {
+                nickMessage.innerText = "이미 사용 중인 닉네임입니다.";
+                nickMessage.classList.add("error");
+                nickMessage.classList.remove("confirm");
+                checkObj.memberNickname = false;
+            }
+        })
     nickMessage.innerText = "유효한 닉네임 형식입니다.";
     nickMessage.classList.add("confirm");
     nickMessage.classList.remove("error");
     checkObj.memberNickname = true;
-
     } else {
         nickMessage.innerText = "닉네임 형식이 유효하지 않습니다.";
         nickMessage.classList.add("error");
@@ -61,35 +56,19 @@ const updateInfo = document.getElementById("updateInfo");
 
 updateInfo.addEventListener("submit", e => {
 
-    // 현재 비밀번호를 입력하지 않을 경우
-    if(currentPw.value.trim() == ""){
-    alert("현재 비밀번호를 입력해주세요");
-    e.preventDefault();
-    currentPw.focus();
-    return;
-    }
-
-    // 현재 비밀번호가 유효하지 않을 경우
-    if(currentPwMessage.classList.contains("error")){
-    alert("비밀번호가 유효하지 않습니다");
-    e.preventDefault();
-    currentPwMessage.focus();
-    return;
-    }
-
     // 닉네임을 입력하지 않았을 경우
     if(memberNickname.value.trim() == ""){
         alert("변경할 닉네임을 입력해 주세요");
-        e.preventDefault();
         memberNickname.focus();
+        e.preventDefault();
         return;
-}
+    }
 
     // 닉네임이 유효하지 않을 경우
     if(nickMessage.classList.contains("error")){
         alert("닉네임이 유효하지 않습니다.");       
+        nickMessage.focus();
         e.preventDefault();
-        nickMessage.focus(); // 기본 이벤트 제거
         return;
     }
 });
