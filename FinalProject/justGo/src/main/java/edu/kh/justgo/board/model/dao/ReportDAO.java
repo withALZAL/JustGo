9035d7c893e81abf49a3922a8ef63e3cdcc0bc44
정송이 <pso7364@naver.com>
2023-07-02 00:24:46 +0900

    회원의 게시물 신고 구현

---
+++ FinalProject/justGo/src/main/java/edu/kh/justgo/board/model/dao/ReportDAO.java
@@ -0,0 +1,27 @@
+package edu.kh.justgo.board.model.dao;
+
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import edu.kh.justgo.manager.model.dto.Report;
+
+
+@Repository
+public class ReportDAO {
+	
+	@Autowired
+	public SqlSessionTemplate sql;
+
+	/** 회원이 게시물 신고
+	 * @param report
+	 * @return
+	 */
+	public int reportBoard(Report report) {
+		return sql.insert("reportMapper.reportBoard", report);
+	}
+
+	
+
+}
