8ff84cf8fcde9ad93b1c5c7a70dbf8f73ebb82f7
sangzoon0102 <sangzoon0102@naver.com>
2023-07-04 17:48:55 +0900

    chartJS

--- FinalProject/justGo/src/main/webapp/resources/js/manager/managerDashboard.js
+++ FinalProject/justGo/src/main/webapp/resources/js/manager/managerDashboard.js
@@ -1,4 +1,4 @@
-/* 도넛 차트 */
+/* 도넛 차트(2번) */
 var chartDoughnut = document.getElementById('chartDoughnut').getContext('2d');
 new Chart(chartDoughnut, {
     type: 'doughnut',
@@ -19,25 +19,70 @@ new Chart(chartDoughnut, {
     }
 });
 
-/* 바 차트 */
-var chartBar = document.getElementById('chartBar').getContext('2d');
-new Chart(chartBar, {
-    type: 'horizontalBar',
+/* 태그 차트(3번) */
+var chartTag = document.getElementById('chartTag');
+
+
+/* 라인 차트(4번) */
+var chartLine = document.getElementById('chartLine');
+new Chart(chartLine, {
+    type: 'line',
     data: {
-        labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+        labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
+        datasets: [{
+            data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
+            label: "Africa",
+            borderColor: "#3e95cd",
+            fill: false
+        }, {
+            data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
+            label: "Asia",
+            borderColor: "#8e5ea2",
+            fill: false
+        }, {
+            data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
+            label: "Europe",
+            borderColor: "#3cba9f",
+            fill: false
+        }, {
+            data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
+            label: "Latin America",
+            borderColor: "#e8c3b9",
+            fill: false
+        }, {
+            data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
+            label: "North America",
+            borderColor: "#c45850",
+            fill: false
+        }
+        ]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'World population per region (in millions)'
+        }
+    }
+});
+
+
+/* 바 차트(5번)*/
+var chartBar = document.getElementById('chartBar');
+new Chart(chartBar, {
+    type: 'bar',
+    data: {
+        labels: ["욕설", "차별적 표현", "사칭", "광고성, 도배글", "선정성", "불법정보포함", "개인정보노출", "기타"],
         datasets: [
             {
-                label: "Population (millions)",
-                backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
-                data: [2478, 5267, 734, 784, 433]
+                backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9"],
+                data: [2478, 5267, 734, 784, 2478, 5267, 734, 784] /* 여기에 데이터를 넣습니다. */
             }
         ]
     },
     options: {
         legend: { display: false },
         title: {
-            display: true,
-            text: 'Predicted world population (millions) in 2050'
+            display: true
         }
     }
 });
