daa41df79a86575d4b6f7493d534fff71bfde7d9
정송이 <pso7364@naver.com>
2023-07-10 20:42:04 +0900

    alert창 toast로 변환

--- FinalProject/justGo/src/main/webapp/resources/js/manager/memberPage.js
+++ FinalProject/justGo/src/main/webapp/resources/js/manager/memberPage.js
@@ -4,10 +4,44 @@ function deleteMemberBtn(memberNo){
     .then(resp => resp.text())
     .then(result => {
         if(result > 0){
-            alert("강퇴 성공!");
+            // alert("강퇴 성공!");
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end', /* 우측 상단 */
+                showConfirmButton: false, /* 컨펌버튼 없음 */
+                timer: 3000, /* 3초 간 뜨기 */
+                timerProgressBar: true, /* 진행바 */
+                showCloseButton: true, /* 취소 버튼 */
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+                })
+        
+                Toast.fire({
+                    icon: 'success', /* 아이디 실패 시 ! 경고 */
+                    title: '강퇴 성공!' /* 메시지 담기 */
+            })
             location.href =  location.href ;
         }else{
-            alert("강퇴 실패...")
+            // alert("강퇴 실패...")
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end', /* 우측 상단 */
+                showConfirmButton: false, /* 컨펌버튼 없음 */
+                timer: 3000, /* 3초 간 뜨기 */
+                timerProgressBar: true, /* 진행바 */
+                showCloseButton: true, /* 취소 버튼 */
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+                })
+        
+                Toast.fire({
+                    icon: 'warning', /* 아이디 실패 시 ! 경고 */
+                    title: '강퇴 실패...' /* 메시지 담기 */
+            })
         }
     })
     .catch(err => console.log(err));
@@ -20,10 +54,44 @@ function restoreMemberBtn(memberNo){
     .then(resp => resp.text())
     .then(result => {
         if(result > 0){
-            alert("계정복구 성공!");
+            // alert("계정복구 성공!");
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end', /* 우측 상단 */
+                showConfirmButton: false, /* 컨펌버튼 없음 */
+                timer: 3000, /* 3초 간 뜨기 */
+                timerProgressBar: true, /* 진행바 */
+                showCloseButton: true, /* 취소 버튼 */
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+                })
+        
+                Toast.fire({
+                    icon: 'success', /* 아이디 실패 시 ! 경고 */
+                    title: '계정복구 성공!' /* 메시지 담기 */
+            })
             location.href =  location.href ;
         }else{
-            alert("계정복구 실패...")
+            // alert("계정복구 실패...")
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end', /* 우측 상단 */
+                showConfirmButton: false, /* 컨펌버튼 없음 */
+                timer: 3000, /* 3초 간 뜨기 */
+                timerProgressBar: true, /* 진행바 */
+                showCloseButton: true, /* 취소 버튼 */
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+                })
+        
+                Toast.fire({
+                    icon: 'warning', /* 아이디 실패 시 ! 경고 */
+                    title: '계정복구 실패...' /* 메시지 담기 */
+            })
         }
     })
     .catch(err => console.log(err));
